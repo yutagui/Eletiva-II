2b62270179a9fb66c8186078b24e9e25e03d43a5
Sistema de Informação <si@LAB03-31.laboratorio.dom>
2024-04-09 20:45:35 -0300

    part 1 02/04/24

--- Salve a Terra - Alunos/js/jogo.js
+++ Salve a Terra - Alunos/js/jogo.js
@@ -7,3 +7,27 @@ setInterval(
     loop,
     30
 );
+
+function start() {
+    $('#alerta').css('display', 'none')
+
+    $('#area_jogo').append('<div id="player"></div>')
+
+    $('#area_jogo').append('<div id="tanque"></div>')
+
+    $('#area_jogo').append('<div id="pessoa"></div>')
+
+    $('#area_jogo').append('<div id="inimigo"></div>')
+
+    $('#area_jogo').append('<div id="explosao"></div>')
+
+    $('#area_jogo').append('<div id="inimigo_explosao"></div>')
+
+    $('#area_jogo').append('<div id="inimigo_tiro"></div>')
+
+    $('#area_jogo').append('<div id="jogador_explosao"></div>')
+
+    $('#area_jogo').append('<div id="tiro"></div>')
+}
+
+start()
